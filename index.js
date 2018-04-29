9712d5bdb77c6d4a48b77a2e351350142bd973aa
Sebastian Meier <contact@sebastianmeier.eu>
2018-04-29 13:43:45 +0200

    nightmare upgrade

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var fs = require('fs'),
   gm = require('gm')
 
 var Nightmare = require('nightmare')
-var nightmare = Nightmare({show:true})
+var nightmare = Nightmare()
 
 var render = (function () {
  
@@ -156,11 +156,6 @@ var render = (function () {
         console.log('nghtmr:snap:forEachPromise', snap_count)
         return new Promise((resolve, reject) => {
           nightmare
-            .evaluate(()=>{
-              setTimeout(()=>{
-                window.scrollBy(0,5)
-              }, 500)
-            })
             .screenshot('.' + job.folder + '/png/' + module.formatNumber(snap_count) + '.png', {x:0,y:0,width:config.video.output.width,height:config.video.output.height})
             .then(() => {
               console.log('nghtmr:snap-scrnsht', snap_count, (snap_count / job.data.params.duration))
@@ -255,11 +250,6 @@ var render = (function () {
               console.log('nghtmr-processSize:screenshot-before')
               return new Promise((resolve, reject) => {
                 nightmare
-                  .evaluate(()=>{
-                    setTimeout(()=>{
-                      window.scrollBy(0,5)
-                    }, 500)
-                  })
                   .screenshot('.' + job.folder + '/social/' + size.file + '.png', {x:0,y:0,width:size.scale.width,height:size.scale.height})
                   .then(function (result) {
                     console.log('nghtmr-processSize:screenshot')
